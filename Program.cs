72bc8f60501ca8c6a9f4016b9fdadbbde0f2cc51
Dragan Pepic <nedimco2006@gmail.com>
2022-01-15 19:39:40 +0100

    bvnbv

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
 ﻿bool izlaz = false;
 do
 {
-    Console.WriteLine("*********************");
-    Console.WriteLine("~~1. Sabiranje~~");
-    Console.WriteLine("~~2. Oduzimanje~~");
-    Console.WriteLine("~~3. Izlaz~~");
-    Console.WriteLine("*********************");
+    StampajMeni();
     Console.Write("Izbor? ");
     string izb = Console.ReadLine();
-
     switch (izb)
     {
         case "1":
@@ -16,7 +11,8 @@ do
             int broj1 = int.Parse(Console.ReadLine());
             Console.Write("Unesi drugi broj: ");
             int broj2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Rezultat je: " + (broj1 + broj2));
+            int zbir = Sabiranje(broj1, broj2);
+            Console.WriteLine("Rezultat je: " + zbir);
             break;
 
         case "2":
@@ -39,3 +35,23 @@ do
     Console.ReadKey();
     Console.Clear();
 } while (!izlaz);
+
+int Sabiranje(int x, int y)
+{
+    return x + y;
+}
+
+//povratni tip          naziv                  ulazni parametri
+void StampajMeni()
+{
+    StampajRed("*********************");
+    StampajRed("~~1. Sabiranje~~");
+    StampajRed("~~2. Oduzimanje~~");
+    Console.WriteLine("~~3. Izlaz~~");
+    Console.WriteLine("*********************");
+}
+
+void StampajRed(string red)
+{
+    Console.WriteLine(red);
+}
